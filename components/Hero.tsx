56caa79d781da67bd229e427e465f7e7b1b7da28
GarethScott007 <gareth007@me.com>
2025-11-09 22:45:33 +0000

    Refactor img tag formatting in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,14 @@ export default function Hero() {
     <section className="my-6">
       <div className="rail">
         <div className="rounded-hero overflow-hidden shadow-xl">
-          <img src="/img/hero-home.jpg" alt="" width={1920} height={860} className="w-full h-auto block" fetchPriority="high" />
+          <img
+            src="/img/hero-home.jpg"
+            alt=""
+            width={1920}
+            height={860}
+            className="w-full h-auto block"
+            fetchPriority="high"
+          />
         </div>
       </div>
     </section>
