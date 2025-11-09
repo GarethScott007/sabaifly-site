dac7b3b592aedf8cce94e94e86fd8231825b2d81
GarethScott007 <gareth007@me.com>
2025-11-09 12:34:04 +0000

    Delete functions directory

--- functions/robots.txt.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest({ request }) {
-  const host = new URL(request.url).hostname.toLowerCase();
-  const isPreview =
-    host.endsWith('.pages.dev') || host.startsWith('stage.') || host.startsWith('staging.');
-  const body = isPreview
-    ? `User-agent: *\nDisallow: /\n`
-    : `User-agent: *\nAllow: /\nSitemap: https://sabaifly.com/sitemap.xml\n`;
-  return new Response(body, { headers: { 'content-type': 'text/plain; charset=utf-8' } });
-}
